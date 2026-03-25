1d9111c8e5a20654076bc196e59f0e7ca46f5dc4
mdshahnawaz123 <41420658+mdshahnawaz123@users.noreply.github.com>
2026-03-25 15:39:19 +0400

    Initial commit of DataLab library

---
+++ DocumentExtension.cs
@@ -0,0 +1,90 @@
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLab
+{
+    public static class DocumentExtension
+    {
+        public static TElement GetElementByName<TElement>(this Document doc, string name )
+            where TElement : Element
+        {
+            var ele = new FilteredElementCollector(doc)
+                .OfClass(typeof(TElement))
+                .FirstOrDefault(x => x.Name == name);
+
+            if( ele == null )
+            {
+                throw new ArgumentNullException(nameof(name), $"Element with name '{name}' not found.");
+            }
+            return (TElement)ele;
+        }
+
+        public static DirectShape CreateDirectShape(this Document doc,List<GeometryObject> geometries , ElementId categoryId)
+        {
+            var ds = DirectShape.CreateElement(doc, categoryId);
+            ds.SetShape(geometries);
+
+            return ds;
+        }
+
+        public static DirectShape CreateDirectShape(this Document doc, GeometryObject geometryObject, ElementId categoryId)
+        {
+            var ds = DirectShape.CreateElement(doc, new ElementId(BuiltInCategory.OST_GenericModel));
+            ds.SetShape(new List<GeometryObject>(){ geometryObject});
+
+            return ds;
+        }
+
+        public static void DoAction(this Document doc, Action action,string name)
+        {
+            using(var tx = new Transaction(doc,name))
+            {
+                tx.Start();
+                action.Invoke();
+                tx.Commit();
+            }
+        }
+
+        public static IList<Level> GetLevel(this Document doc)
+        {
+            return new FilteredElementCollector(doc)
+                .OfClass(typeof(Level))
+                .Cast<Level>()
+                .ToList();
+        }
+
+        public static IList<Category> GetCategory(this Document doc)
+        {
+            var cat = doc.Settings.Categories
+                .Cast<Category>()
+                .Where(x =>x.CategoryType == CategoryType.Model && !x.IsTagCategory)
+                .OrderBy(x => x.Name)
+                .ToList();
+
+            return cat;
+        }
+
+        public static IList<Element> GetElementByCategory(this Document doc, Category category)
+        {
+            return new FilteredElementCollector(doc)
+                .OfCategoryId(category.Id)
+                .WhereElementIsNotElementType()
+                .ToElements();
+        }
+
+        public static IList<Element> GetElementTypeByCategory(this Document doc, Category category)
+        {
+            return new FilteredElementCollector(doc)
+                .OfCategoryId(category.Id)
+                .Cast<Element>()
+                .ToList();
+
+        }
+    }
+}
