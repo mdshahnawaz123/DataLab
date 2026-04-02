0c33483108a532868532d4c4f13dddd7ac2b9771
mdshahnawaz123 <41420658+mdshahnawaz123@users.noreply.github.com>
2026-04-02 15:47:09 +0400

    User Lic added

---
+++ Resources/ImageUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DataLab.Resources
+{
+    public static class ImageUtils
+    {
+        public static ImageSource? GetEmbeddedImage(string resourceName)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null) return null;
+
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.StreamSource = stream;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.EndInit();
+                image.Freeze();
+                return image;
+            }
+        }
+    }
+}
